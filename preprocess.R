93fc6d5f945a1a4f6b71e3132216178d88272340
Marcel Ribeiro-Dantas <marcel.ribeiro-dantas@curie.fr>
2020-03-01 18:36:47 +0100

    Adds preprocessing script/pipeline entry

---
+++ preprocess.R
@@ -0,0 +1,8 @@
+input_file <- read.csv(file = 'data/simulation.tsv', sep='\t', stringsAsFactors=FALSE)
+input_file <- input_file[, 1:50]
+
+input_file[, unlist(lapply(input_file, is.numeric))] <- apply(input_file[, unlist(lapply(input_file, is.numeric))], 2, function(x) ifelse(x < 0, -1, x))
+
+input_file[, unlist(lapply(input_file, is.numeric))] <- apply(input_file[, unlist(lapply(input_file, is.numeric))], 2, function(x) ifelse(x > 0, 1, x))
+
+write.csv2(input_file, 'data/simulation_preprocessed.csv', row.names=FALSE)
